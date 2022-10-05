0190b2d3c31a8b2239ececf1defbc514566a1962
Lewis Lin <linle211@gmail.com>
2022-10-04 20:13:33 -0700

    Fixed Login
    Co-authored-by: Pei Chu <pchu2018@gmail.com>

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dashboard from '../containers/Dashboard';
+import Login from './Login';
+import { Link, redirect, Route, Routes } from 'react-router-dom';
 
 import '../stylesheets/main.scss';
 
 function App() {
-  return (
-    <div className='app-container'>
-      <Dashboard />
-    </div>
-  )
+    return <Dashboard/>
 }
 
 export default App;
