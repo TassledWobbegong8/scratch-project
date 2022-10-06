2c58b47b20a5bbdf9c4aa4c039102277f1e0e8e4
umas15 <umas15@gmail.com>
2022-10-05 21:28:55 -0400

    initial oauth setup

--- server/server.js
+++ server/server.js
@@ -4,6 +4,15 @@ const express = require('express');
 
 const apiRouter = require("./routes/api");
 
+/////////////////////
+const google = require('googleapis').google;
+const jwt = require('jsonwebtoken');
+const OAuth2 = google.auth.OAuth2;
+const CONFIG = require('./config');
+const cookieParser = require('cookie-parser');
+///////////////////////
+
+
 // require("mongoose-type-url");
 
 const PORT = 3000;
@@ -24,6 +33,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+///////////////////////
+app.use(cookieParser());
+//////////////////////
+
+
 // placeholder route for production 
 // app.use('/build', express.static(path.join(__dirname, '../build')));
 
@@ -31,6 +45,19 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
+
+app.get('/auth', (req, res) => {
+
+  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0])
+  if (req.query.error) { console.log(req.query.error) }
+  const loginLink = oauth2Client.generateAuthUrl({
+    access_type: 'offline', // Indicates that we need to be able to access data continously without the user constantly giving us consent
+    scope: CONFIG.oauth2Credentials.scopes // Using the access scopes from our config file
+  });
+  console.log(loginLink)
+  return res.status(200).json(loginLink)
+})
+
 
 app.use("/api", apiRouter);
 
