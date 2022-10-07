3c8439c7e33f57f4791996ca520655598698577b
umas15 <umas15@gmail.com>
2022-10-07 13:00:52 -0400

    completed oauth for google drive api and able to fetch documents list from drive

--- server/server.js
+++ server/server.js
@@ -5,13 +5,14 @@ const express = require('express');
 const apiRouter = require('./routes/api');
 
 /////////////////////
-const google = require('googleapis').google;
+const { google } = require('googleapis');
 const jwt = require('jsonwebtoken');
 const OAuth2 = google.auth.OAuth2;
 const CONFIG = require('./config');
 const cookieParser = require('cookie-parser');
-const { JWTsecret, oauth2Credentials } = require('./config');
-const { drive } = require('googleapis/build/src/apis/drive');
+
+const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
+let jwt_token;
 ///////////////////////
 
 
@@ -48,44 +49,56 @@ app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../index.html'));
 });
 
+
+
 app.get('/auth', (req, res) => {
 
-  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
-  if (req.query.error) { console.log(req.query.error); }
   const loginLink = oauth2Client.generateAuthUrl({
     access_type: 'offline',
-    scope: CONFIG.oauth2Credentials.scopes
+    prompt: 'consent',
+    scope: CONFIG.oauth2Credentials.scopes,
+    include_granted_scopes: true
   });
-  console.log(loginLink);
+  // console.log(loginLink);
   return res.status(200).json(loginLink);
 });
 
-app.get('/auth_callback', (req, res) => {
-  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
-  if (req.query.error) {
-    console.log(req.query.error);
-    return res.redirect('/');
-  } else {
-    const token = oauth2Client.getToken(req.query.code);
-    res.cookie('gjwt', jwt.sign(token, JWTsecret));
+app.get('/auth_callback', async (req, res) => {
+  try {
+
+    const { tokens } = await oauth2Client.getToken(req.query.code);
+    oauth2Client.setCredentials(tokens);
+
+    jwt_token = jwt.sign(tokens.refresh_token, CONFIG.JWTsecret);
+
     return res.redirect('/access_drive');
 
+  } catch (e) {
+    console.log(e.message);
   }
 });
 
-app.get('/access_drive', (req, res) => {
-  if (!res.cookies.gjwt) return res.redirect('/');
-  const oauth2Client = new OAuth2(CONFIG.oauth2Credentials.client_id, CONFIG.oauth2Credentials.client_secret, CONFIG.oauth2Credentials.redirect_uris[0]);
-  oauth2Client.credentials = jwt.verify(res.cookies.gjwt, JWTsecret);
-  const service = google.drive('v3');
-  drive.files.list({}, (err, res) => {
-    if (err) throw err;
-    const files = res.data.files;
-    if (files.length) {
-      files.map(e => console.log(e));
-    } else { console.log('No Files Found'); }
-  });
 
+app.get('/access_drive', async (req, res) => {
+
+  const decoded = jwt.verify(jwt_token, CONFIG.JWTsecret);
+  const drive = google.drive('v3');
+  let response;
+  const fileArray = [];
+  try {
+    response = await drive.files.list({
+      auth: oauth2Client,
+      pageSize: 10,
+      fields: 'nextPageToken, files(id, name)'
+    });
+
+  } catch (e) {
+    console.log('Error from API:', e.message);
+  }
+  if (response.data.files.length) {
+    response.data.files.forEach(e => { fileArray.push(e.name); });
+  } else { return res.status(400).json('There were no documents found on this drive'); }
+  return res.status(200).json(fileArray);
 });
 
 
