adb06ae0142fe4d40d098523a1b8d91725533005
PEIYUN CHU <pchu2018@gmail.com>
2022-10-07 22:12:15 -0400

    updated server routes for oauth, token stored cookie

--- client/components/DocumentEditor.jsx
+++ client/components/DocumentEditor.jsx
@@ -3,21 +3,36 @@ import { Button } from '@mui/material';
 import FilePicker from './FilePicker';
 
 function DocumentEditor({ hostView }) {
+  // some fake data for rendering purposes
+  const fakeFileList = ['test1', 'someDocument', 'my story'];
+
   const [openPicker, setPicker] = useState(false);
+  const [fileList, setFiles] = useState(fakeFileList);
+  const [document, setDocument] = useState({});
 
   const connectAuth = async () => {
+    console.log('click auth');
+    // first check to see if token is already in cookies
+    const files = await fetch('/access_drive').then(response => response.json());
+
+    if (files) {
+      console.log(files)
+      return setFiles(files);
+    }
     // request oauth url from server
     const redirectURL = await fetch('/auth').then(response => response.json());
     // redirect user to consent screen
-    window.location.replace(redirectURL);
+    const redirect = window.location.replace(redirectURL);
+    // redirect should come back as an array with the files
+    setFiles(await redirect.json());
+    setPicker(true);
   };
 
 
   return (
     <div className='doc-editor'>
-      {hostView ? 
-        <Button onClick={() => connectAuth()}>Choose a Google Drive File</Button>
-        : 'host document here'}
+      {openPicker && <FilePicker fileList={fileList} setDocument={setDocument} />}
+      {hostView && <Button onClick={() => connectAuth()}>Choose a Google Drive File</Button>}
     </div>
   );
 }
--- client/components/RoomCard.jsx
+++ client/components/RoomCard.jsx
@@ -19,8 +19,8 @@ function RoomCard( { info, id } ) {
 
   async function saveRoom () {
     const options = {method: 'PATCH', headers: {'Content-Type': 'application/json'}, body: JSON.stringify({'savedRooms': `${info._id}`})};
-    await fetch(`/api/users/${id}/saveroom`, options);
-    console.log("Room Saved!");
+    await fetch('/api/users/saveroom', options);
+    console.log('Room Saved!');
   }
 
   const showRoomInfo = event => {
