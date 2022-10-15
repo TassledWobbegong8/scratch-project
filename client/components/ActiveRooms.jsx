38e7607cd54a9d262420b430cd5cdead146e97c6
yardenedry <96927338+yardenedry@users.noreply.github.com>
2022-10-15 13:25:39 -0400

    Yarden-10-14-post-it-home (#10)
    * Frontend Fixes
    
    * grid creation
    
    * navbar fix, room exit btn, settings bar vh fix
    
    * post-it card style update

--- client/components/ActiveRooms.jsx
+++ client/components/ActiveRooms.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 const ActiveRooms = () => {
 
-  const [ Test1, Test2, Test3, Test4] = 'Testing';
-  const rooms = [Test1, Test2, Test3, Test4];
+  const [ Test1, Test2, Test3, Test4, Test5, Test6, , Test7, Test8] = 'Testing';
+  const rooms = [Test1, Test2, Test3, Test4, Test5, Test6, Test7, Test8];
 
   return (
 
@@ -16,18 +16,22 @@ const ActiveRooms = () => {
         {rooms.map((rooms, i) => (
           <div className='grid-item' key={i}>
             <div className='grid-item-front'>
-              Heres some text
+              <p>Room #{i + 1}</p>
+
+              <a href='/main/home'>
+                <button className='join-btn'>JOIN</button>
+              </a>
+
             </div>
             {/* hover effect */}
-            <div className='grid-item-back'>
+            {/* <div className='grid-item-back'>
               <h2>Testing Card: Back</h2>
               <div className='hover-btns'>
                 <a href='/main/home'>
                   <button>Join Room</button>
                 </a>
               </div>
-            </div>
-
+            </div> */}
           </div>
         ))}
 
