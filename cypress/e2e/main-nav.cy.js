5cda73c0fce515736b3c9ba8dfd485014ad5a199
Christopher Kim <101858037+christophminkim@users.noreply.github.com>
2022-10-15 14:45:33 -0700

    add test cases in main-nav.cy.js

--- cypress/e2e/main-nav.cy.js
+++ cypress/e2e/main-nav.cy.js
@@ -1,38 +1,47 @@
 /// <reference types="cypress" />
 
 context('main navbar', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('http://localhost:8080/');
     cy.get('input').first().type('test');
     cy.get('input').last().type('testtest');
     cy.get('button').contains('Login').click();
   });
-  specify('should have main navbar', () => {
+  specify('should have main navbar and each button should take you to the proper page', () => {
     cy.get('button#home-link').should('exist').and('have.text', 'Home');
-    cy.get('button#profile-link').should('exist').and('have.text', 'Profile');
-    cy.get('button#settings-link').should('exist').and('have.text', 'Settings');
     cy.get('button#logout-link').should('exist').and('have.text', 'Logout');
-  });
 
-  specify('home button should take you to the homepage', () => {
-    cy.get('button#profile-link').click();
-    cy.get('button#home-link').click();
+    cy.get('button#profile-link').should('exist').and('have.text', 'Profile').click();
+    cy.location('pathname').should('eq', '/main/profile');
+    cy.get('button#home-link').should('exist').and('have.text', 'Home').click();
+    cy.location('pathname').should('eq', '/main/home');
+
+    cy.get('button#settings-link').should('exist').and('have.text', 'Settings').click();
+    cy.location('pathname').should('eq', '/main/settings');
+    cy.get('button#home-link').should('exist').and('have.text', 'Home').click();
     cy.location('pathname').should('eq', '/main/home');
   });
-  specify.only('profile button should take you to the profile page', () => {
+
+  specify('profile should have your name and username rendered based on your login', () => {
     cy.get('button#profile-link').click();
     cy.location('pathname').should('eq', '/main/profile');
 
     cy.get('h1').should('have.text', 'test-user');
     cy.get('p.profile-field').should('have.text', '@test');
-
-    cy.get('button#saved-rooms-tab').should('have.text', 'Saved Rooms').click();
-    cy.get
-
-
-    cy.get('button#profile-tab').should('have.text', 'Room Manager');
-
   });
+
+  // specify.only('button in profile/room manager tab should create a new room', () => {
+  //   cy.get('button#profile-link').click();
+  //   cy.location('pathname').should('eq', '/main/profile');
+
+  //   cy.get('button#open-add-room').click();
+    
+  // });
+
+  // cy.get('button#profile-tab').should('have.text', 'Room Manager').click();
+
+  // cy.get('button#saved-rooms-tab').should('have.text', 'Saved Rooms').click();
+
   specify('settings button should take you to the settings page', () => {
     cy.get('button#settings-link').click();
     cy.location('pathname').should('eq', '/main/settings');
