1dc71650544b41dc587a066be395df424cf0e84a
zezang <zezang@gmail.com>
2022-10-16 23:26:24 -0700

    Merged backend changes and added access control allow credentials to response headers
    Co-authored-by: pchu2018 <pchu2018@gmail.com>
    Co-authored-by: hxdr <raymondhuang2015@gmail.com>
    Co-authored-by: WarmDarkMatter <yirou.zm@gmail.com>
    Co-authored-by: MarikoIwata <3701668+MarikoIwata@users.noreply.github.com>
    Co-authored-by: MarikoIwata <3701668+MarikoIwata@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -29,6 +29,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+});
 //////////////////////
 
 
