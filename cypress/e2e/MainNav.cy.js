88e79c626a578d69406af74cc2db9b45a3f79145
Terry Park <terrypark@terrys-air.lan>
2022-10-17 17:26:23 -0700

    cypress testing

--- cypress/e2e/MainNav.cy.js
+++ cypress/e2e/MainNav.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Rooms', () => {
+describe('Main/Top Navigation Bar', () => {
   
   beforeEach(() => {
     cy.visit('http://localhost:8080/')
@@ -13,37 +13,5 @@ describe('Rooms', () => {
     cy.get("[id='home-link']").click()
     cy.get("[id='main-container']").get("[id='no-subject']").should("be.visible")
   })
-
-  it('Should return the Math rooms', () => {
-    cy.get("[id='subject-links-container']").contains("math").click()
-    cy.get("[id='room-container']").contains('math').should("be.visible")
-  })
-
-  it('Should return the English rooms', () => {
-    cy.get("[id='subject-links-container']").contains("english").click()
-    cy.get("[id='room-container']").contains('english').should("be.visible")
-  })
-
-  it('Should return the History rooms', () => {
-    cy.get("[id='subject-links-container']").contains("history").click()
-    cy.get("[id='room-container']").contains('history').should("be.visible")
-  })
-
-  it('Should return the Science rooms', () => {
-    cy.get("[id='subject-links-container']").contains("science").click()
-    cy.get("[id='room-container']").contains('science').should("be.visible")
-  })
-
-  it('Should return the Language rooms', () => {
-    cy.get("[id='subject-links-container']").contains("languages").click()
-    cy.get("[id='room-container']").contains('languages').should("be.visible")
-  })
-
-  it('Should return the correct subject', () => {
-    cy.get("[id='subject-links-container']").contains('miscellaneous').click()
-    cy.get("[id='room-container']").contains('miscellaneous').should("be.visible")
-  })
-  
-
 
 })
